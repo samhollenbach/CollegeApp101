71d9d3aaab3b0e39890891d83f8a72c483d95c9b
Sam Hollenbach <shollenb@macalester.edu>
2017-02-26 23:46:41 -0600

    Initial Commit

---
+++ College App 101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College App 101
+//
+//  Created by Sam Hollenbach on 2/26/17.
+//  Copyright © 2017 Sam Hollenbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
