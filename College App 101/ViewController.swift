9c8c96632526a4306e1ec2065a4a533300dcaba1
Eric Ota <theericota@gmail.com>
2017-03-07 00:05:53 -0800

    Added The Hub, Coaching, and Achievement Tracker.  Other changes too.

--- College App 101/ViewController.swift
+++ College App 101/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainScroll: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let vc0 = CoachingVC(nibName: "CoachingVC", bundle: nil)
+        self.addChildViewController(vc0)
+        self.mainScroll.addSubview(vc0.view)
+        vc0.didMove(toParentViewController: self)
+        
+        let vc1 = TheHubVC(nibName: "TheHubVC", bundle: nil)
+        var frame1 = vc1.view.frame
+        frame1.origin.x = self.view.frame.size.width
+        vc1.view.frame = frame1
+        self.addChildViewController(vc1)
+        self.mainScroll.addSubview(vc1.view)
+        vc1.didMove(toParentViewController: self)
+        
+        let vc2 = AchievementTrackerVC(nibName: "AchievementTrackerVC", bundle: nil)
+        var frame2 = vc2.view.frame
+        frame2.origin.x = self.view.frame.size.width * 2
+        vc2.view.frame = frame2
+        self.addChildViewController(vc2)
+        self.mainScroll.addSubview(vc2.view)
+        vc1.didMove(toParentViewController: self)
+        
+        self.mainScroll.contentSize = CGSize(width: self.view.frame.size.width * 3, height: self.view.frame.size.height)
+        self.mainScroll.contentOffset.x = self.view.frame.size.width
+
+        
     }
 
     override func didReceiveMemoryWarning() {
