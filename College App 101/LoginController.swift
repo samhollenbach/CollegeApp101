d814b9dfa4a1e0b71611962d9289ff61d3f8b224
Eric Ota <theericota@gmail.com>
2017-07-08 16:17:20 -0700

    SINE TGUBG

--- College App 101/LoginController.swift
+++ College App 101/LoginController.swift
@@ -29,14 +29,5 @@ class LoginController: UIViewController {
         //LOGIN PRESSED, CHECK IF USERNAME AND PASSWORD ARE A LEGIT ACOUNT, THEN LOGIN
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
